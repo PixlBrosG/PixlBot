744f09ba215167c69e6514cde49fac940f05cc4c
PixlBrosG <pixelbrosg@gmail.com>
2022-07-08 19:31:54 +0200

    Version 4.1.1

--- src/commands/queue.js
+++ src/commands/queue.js
@@ -8,35 +8,29 @@ export const permissions = [];
 import { songQueue } from '../index.js';
 import { Embed, ParseVideo } from '../API.js';
 
+function queueEntry(song)
+{
+	return ParseVideo(song) + `\`${song.length}\``;
+}
+
 export function execute(msg, _)
 {
 	let queue = songQueue.get(msg.guild.id);
-	let song  = queue.songs[0];
 
 	if (!queue)
 		return 'Server queue is empty';
 
 	let embed = Embed(msg.author).setTitle(`Song queue (${queue.songs.length - 1} tracks)`)
-		.setThumbnail(song.thumbnailURL)
-		.setDescription(`**Currently playing**:\n${ParseVideo(song)} \`${song.length}\``);
+		.setThumbnail(queue.songs[0].thumbnailURL)
+		.setDescription(`**Currently playing**:\n${queueEntry(queue.songs[0])}`);
 	embed.footer.text += ` - Replay: ${queue.replay ? '✅' : '❌'}`;
 
 	if (queue.songs.length > 1)
 	{
-		let indexes = [];
-		let tracks  = [];
-		let lengths = [];
+		embed.description += '\n';
 
 		for (let i = 1; i < queue.songs.length; ++i)
-		{
-			indexes.push(`\`${i}\``);
-			tracks.push(ParseVideo(queue.songs[i]));
-			lengths.push(`\`${queue.songs[i].length}\``);
-		}
-
-		embed.addField('Index', indexes.join('\n'), true);
-		embed.addField('Track', tracks.join('\n'), true);
-		embed.addField('Length', lengths.join('\n'), true);
+			embed.description += `\n\`${i}\` ${queueEntry(queue.songs[i])}`;
 	}
 
 	msg.channel.send({ embeds: [embed] });
